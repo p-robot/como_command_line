b5cfaab3fef7f2de768e10533f91c2b5d0eb0a2c
p-robot <will@probert.co.nz>
2021-05-03 16:38:01 +0100

    Adjust cpp test to only test the cpp version

--- tests/test_cpp_noncpp_app.R
+++ tests/test_cpp_noncpp_app.R
@@ -40,22 +40,3 @@ for(col in cols2check){
   expect_equal(df_app[[col]], df_cli_cpp[[col]])
 }
 print("All baseline CPP output passed")
-
-###########################
-# RUN THE MODEL WITHOUT CPP
-# -------------------------
-source("R/como_preamble.R")
-source("R/model_once.R")
-source("R/como_functions.R")
-source("R/fun_covid.R") # solution function for deSolve (no CPP)
-
-print("Checking non-CPP output")
-USE_CPP <- FALSE
-list_template <- load_template(file_path, country_name, USE_CPP)
-list_output <- run_model(list_template)
-df_cli_nocpp <- process_outputs(list_output, list_template)
-
-for(col in cols2check){
-  expect_equal(df_app[[col]], df_cli_nocpp[[col]])
-}
-print("All baseline non-CPP output passed")
