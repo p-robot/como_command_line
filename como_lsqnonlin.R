4e54f07aeaece6e188376729fd6b17804f5aafa2
p-robot <will@probert.co.nz>
2021-06-30 14:14:50 +0000

    Update como_lsqnonlin.R

--- como_lsqnonlin.R
+++ como_lsqnonlin.R
@@ -3,12 +3,14 @@
 # Script to solve the CoMo model using non-linear least-squares 
 # - Levenberg-Marquardt algorithm (lsqnonlin from pracma package)
 # - Nelder-Mead algorithm (default for optim() function from stats package)
+# stats::constrOptim()
+# 
+# W. Probert
 
 ############
 # PREAMBLE
 # ---------
 
-library(deSolve)
 library(pracma) # for lsqnonlin()
 
 # Define sample data
@@ -21,7 +23,6 @@ output_dir <- "tests/data/"
 df_obs <- read.csv("tests/data/COVID19_App_Data_Template_CoMoCOVID-19App_v17_p0.0245.csv")
 
 sum_stat_obs <- df_obs$baseline_predicted_reported_and_unreported_med
-
 
 ###################
 # MODEL DEFINITION
@@ -37,10 +38,11 @@ como_model <- function(params){
 	n_params <- NROW(df_params)
 
 	list_template <- load_template(file_path, country_name, USE_CPP)
+	print(params)
 	
 	# Adjust parameters
 	for(i in 1:n_params){
-		list_template$parameters[df_params$parameter_name[i]] <- params[i+1]
+		list_template$parameters[df_params$parameter_name[i]] <- params[i]
 	}
 
 	list_output <- run_model(list_template)
@@ -50,13 +52,18 @@ como_model <- function(params){
 	return( sum_stat_sim - sum_stat_obs)
 }
 
+set.seed(2021)
+pars_init <- c(runif(1, 0, 0.1), runif(1, 0, 1.5))
+print(pars_init)
+#lsqnonlin(como_model, pars_init)
 
-pars_init <- c(0.1, 1.0)
-lsqnonlin(como_model, pars_init)
+nlminb(pars_init, function(x) sum(como_model(x)**2), lower = c(0.001, 0.01), upper = c(0.1, 1.5))
 
-optim(pars_init, function(x) sum(como_model(x)**2), method = "Nelder-Mead")
+#optim(pars_init, function(x) sum(como_model(x)**2), method = "L-BFGS-B", lower = c(0, 0), upper = c(0.1, 1.5))
 
-optim(pars_init, function(x) sum(lv_solve(x)**2), method = "BFGS")
+#optim(pars_init, function(x) sum(como_model(x)**2), method = "Nelder-Mead")
+
+#optim(pars_init, function(x) sum(lv_solve(x)**2), method = "BFGS")
 
 # Very slow
 # optim(pars_init, function(x) sum(lv_solve(x)**2), method = "SANN", control = list(verbose = 1))
