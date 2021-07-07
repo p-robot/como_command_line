cf98184a4712621fa997137b35480d543417ec71
p-robot <will@probert.co.nz>
2021-07-07 16:10:18 +0100

    Update como_parallel.R to remove comments related to ABC, to remove UK-specific test runs, to add command-line arguments

--- como_parallel.R
+++ como_parallel.R
@@ -1,34 +1,38 @@
 #!/usr/bin/env Rscript
 # 
-# Script to run an ABC calibration using the CoMo model
-# and the EasyABC package
+# Script to run the CoMo model in parallel across a grid of parameter values
 
 library(parallel)
 library(foreach)
 library(doParallel)
 library(benchmarkme)
 
-output_dir <- "results"
-df_params <- read.csv("parameters/lebanon_parameters.csv", stringsAsFactors = FALSE)
+args <- commandArgs(trailingOnly = TRUE)
+
+# Parse command-line arguments
+country_name <- args[1]
+file_path <- args[2]
+parameters_csv <- args[3]
+output_dir <- args[4]
+
+USE_CPP <- TRUE
+n_cores <- 3
+
+df_params <- read.csv(parameters_csv, stringsAsFactors = FALSE)
 n_params <- NCOL(df_params)
 
-n_cores <- 3
 clust <- parallel::makeCluster(n_cores, type = "PSOCK")
 doParallel::registerDoParallel(cl = clust)
 
 foreach(i = 1:NROW(df_params)) %dopar% {
 	cat("Run ", i, "\n")
 
-	file_path <- "tests/data/Template_CoMoCOVID-19App_v17.xlsx"
-	country_name <- "United Kingdom of Great Britain"
-	USE_CPP <- TRUE
-
 	source("R/como_preamble.R")
 	source("R/model_once.R")
 	source("R/como_functions.R")
 
-        list_template <- load_template(file_path, country_name, USE_CPP)
-	    
+	list_template <- load_template(file_path, country_name, USE_CPP)
+	
 	# Adjust parameters
 	for(j in 1:n_params){
 		param_name <- names(df_params)[j]
@@ -40,19 +44,12 @@ foreach(i = 1:NROW(df_params)) %dopar% {
 	list_output <- run_model(list_template)
 	df_sim <- process_outputs(list_output, list_template)
 
-	write.csv(df_sim, file.path(output_dir, paste0("output_row", i, ".csv")), row.names = FALSE)
+	write.csv(df_sim, file.path(output_dir, paste0("como_output_row", i, ".csv")), 
+        row.names = FALSE, quote = FALSE)
 }
 
 # Stop the cluster
 parallel::stopCluster(cl = clust)
-
-# Add additional information to the ABC object that was run
-#ABC_Lenormand$abc_method <- abc_method
-#ABC_Lenormand$calibrated_parameters <- df_params
-#ABC_Lenormand$n_cluster <- Ncores
-#ABC_Lenormand$nb_simul <- nbsimul
-#ABC_Lenormand$p_acc_min <- pacc
-#ABC_Lenormand$prior <- priors
 
 # Today's date
 today <- format(Sys.time(), "%Y_%m_%d_%H%M")
@@ -73,5 +70,3 @@ system_info <- list(
 	repo_remote = system("git remote get-url origin", intern = TRUE))
 
 save(system_info, file = file.path(output_dir, paste0(today, "_abc_output.Rdata")))
-
-
