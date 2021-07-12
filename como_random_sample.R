d79db4da849d3524be89d6cc34727633627ff7b0
p-robot <will@probert.co.nz>
2021-07-12 11:46:30 +0100

    Add param_id column to the random sample of parameters

--- como_random_sample.R
+++ como_random_sample.R
@@ -12,14 +12,21 @@ if( !interactive() ){
 	n_samples <- as.integer(args[3])
 	output_csv <- args[4]
 	
+	# Set the seed for the random number generator
 	set.seed(seed)
 
+	# Read the CSV file of parameters to adjust
 	df_params <- read.csv(parameter_file, stringsAsFactors = FALSE)
 	n_params <- NROW(df_params)
 
 	sample_vec <- runif(n_samples*n_params, df_params$lower_limit, df_params$upper_limit)
 	df_sample <- as.data.frame(matrix(sample_vec, ncol = n_params, byrow = TRUE))
 	names(df_sample) <- df_params$parameter_name
+
+	# Add a parameter ID to the sample
+        df_sample$param_id <- 1:NROW(df_sample)
+
+	# Write the sample parameter set to a CSV file
 	write.csv(df_sample, output_csv, row.names = FALSE, quote = FALSE)
 }
 
